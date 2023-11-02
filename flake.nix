680516040cd1f4b9f634b2abb660d75099828073
Compact Hermitian <compacthermit@proton.me>
2023-11-02 19:45:38 -0400

    Init Project

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Oxocarbon GTK-ified";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    parts.url = "github:Hercules-CI/flake-parts";
+  };
+
+  outputs = inputs@{ parts, ... }:
+    parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "oxocarbon-gtk";
+          version = "0.0.1";
+
+          src = ./.;
+
+          sourceRoot = ".";
+
+          propagatedUserEnvPkgs = [ pkgs.gtk-engine-murrine ];
+
+          installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/themes
+          cp -a ./* $out/share/themes
+          runHook postInstall
+          '';
+        };
+      };
+    };
+}
